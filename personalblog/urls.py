609c6e828c11725c743453cdc45438b94a7bd45f
Ludceriani <ludmilaceriani1@gmail.com>
2022-04-20 12:02:50 -0300

    create home

---
+++ personalblog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#from . import views
+from .views import ArticleDetailView, HomeView
+
+urlpatterns = [
+    
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail")
+    
+]
